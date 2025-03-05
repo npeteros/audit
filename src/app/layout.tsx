64c5c59c37a3dbabc668ed0016e7960ecc961ebe
Seaside <n.peteros2003@gmail.com>
2025-03-05 12:48:48 +0800

    feat: added responsive navbar

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Roboto_Condensed } from "next/font/google";
+import Navbar from "./_components/navbar";
+import "./globals.css";
+
+const robotoCondensed = Roboto_Condensed({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700"],
+  variable: "--font-roboto-condensed",
+});
+
+export const metadata: Metadata = {
+  title: "Audit PH",
+  description: "A digital archive for all articles of Today's Carolinian",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={`${robotoCondensed.variable} bg-[#F8F8F8]`}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
