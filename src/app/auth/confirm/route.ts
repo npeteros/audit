62353b96d183131511dac2c63b59302e7bd9a344
Seaside <n.peteros2003@gmail.com>
2025-03-13 01:16:32 +0800

    feat: added user auth

---
+++ src/app/auth/confirm/route.ts
@@ -0,0 +1,36 @@
+import { type EmailOtpType } from '@supabase/supabase-js'
+import { type NextRequest } from 'next/server'
+
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
+import AuthService from '@/services/auth.service'
+
+export async function GET(request: NextRequest) {
+    const { searchParams } = new URL(request.url)
+    const token_hash = searchParams.get('token_hash')
+    const type = searchParams.get('type') as EmailOtpType | null
+    const next = searchParams.get('next') ?? '/'
+
+    if (token_hash && type) {
+        const supabase = await createClient()
+
+        const { data: { user }, error } = await supabase.auth.verifyOtp({
+            type,
+            token_hash,
+        })
+        if (!error && user && user.email) {
+            const existingUser = await AuthService.userInDatabase({ email: user.email });
+
+            if (!existingUser) {
+                await AuthService.createUser({ id: user.id, email: user.email })
+            }
+
+            // redirect user to specified redirect URL or root of app
+            redirect(next)
+        }
+
+    }
+
+    // redirect the user to an error page with some instructions
+    redirect('/error')
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,30 @@
 import type { Metadata } from "next";
 import { Roboto_Condensed } from "next/font/google";
-import Navbar from "./_components/navbar";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const robotoCondensed = Roboto_Condensed({
-  subsets: ["latin"],
-  weight: ["100", "300", "400", "700"],
-  variable: "--font-roboto-condensed",
+    subsets: ["latin"],
+    weight: ["100", "300", "400", "700"],
+    variable: "--font-roboto-condensed",
 });
 
 export const metadata: Metadata = {
-  title: "Audit PH",
-  description: "A digital archive for all articles of Today's Carolinian",
+    title: "Audit PH",
+    description: "A digital archive for all articles of Today's Carolinian",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="dark">
-      <body className={`${robotoCondensed.variable} bg-[#F8F8F8]`}>
-        <Navbar />
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" className="dark">
+            <body className={`${robotoCondensed.variable} bg-[#F8F8F8]`}>
+                {children}
+                <Toaster />
+            </body>
+        </html>
+    );
 }
